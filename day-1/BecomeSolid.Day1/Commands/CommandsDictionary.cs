196ebfcd5c17336abc1e52cf1ffe9cbbeb6cd05a
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-11-13 09:44:18 +0300

    Dictionary with Commands

---
+++ day-1/BecomeSolid.Day1/Commands/CommandsDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BecomeSolid.Day1.Commands
+{
+    public class CommandsDictionary
+    {
+        private Dictionary<string, ICommand> dictionary;
+
+        public CommandsDictionary()
+        {
+            dictionary = new Dictionary<string, ICommand>();
+        }
+
+        public void AddCommand(string keyword, ICommand command)
+        {
+            dictionary.Add(keyword, command);
+        }
+
+        public void RemoveCommand(string keyword)
+        {
+            dictionary.Remove(keyword);
+        }
+
+        public bool IsCommandExist(string keyword)
+        {
+            dictionary.ContainsKey(keyword);
+        }
+    }
+}
